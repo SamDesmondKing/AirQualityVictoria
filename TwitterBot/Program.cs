501d8f145fc5cccf76aa4af2d6fce34378ec116e
Sam King <samuel-king@live.com.au>
2020-01-05 01:01:00 +1100

    Added check to ensure no duplicate tweets

--- TwitterBot/Program.cs
+++ TwitterBot/Program.cs
@@ -1,34 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using TweetSharp;
-using System.Timers;
 
 namespace TwitterBot
 {
     class Program
     {
         //TODO 
-        // - Search only EPA_Victoria rather than the whole hashtag
+        // - Automate to update every hour while running on raspi. 
 
+        private static string lastTweet = "";
         private static string customerKey = "gQFaDYO5a59nf3AGWo6ZCCIaJ";
         private static string customerKeySecret = "bbmjE1aU1fQeyvNUR1MpghhyqaUwzWiNlNG4u6HIAcCdD3pJRY";
         private static string accessToken = "1212970147939381250-7aj2fMVVd5yq5HElbElncVOVJkUVJp";
         private static string accessTokenSecret = "joz6Ruap7aV2URSSp7CvGiD6wCs1aZeiCj1iDr5PHcDrr";
-
         private static TwitterService service = new TwitterService(customerKey, customerKeySecret, accessToken, accessTokenSecret);
 
         static void Main(string[] args)
         {
 
             Console.WriteLine($"<{DateTime.Now}> - Bot Started");
+            
+            //Get tweet to send as string 
             var tweet = "RT @EPA_Victoria: " + Program.GetTweet("@EPA_Victoria #AirQuality forecast for today:");
-            //SendTweet(tweet);
-            Console.WriteLine(tweet);
-            Console.Read();
+
+            //Check to make sure we're not sending a duplicate tweet
+            if (tweet != lastTweet)
+            {
+                //Send it
+                //SendTweet(tweet);
+                //Program.lastTweet = tweet;
+                Console.WriteLine(tweet);
+                Console.Read();
+            }
+            else
+            {
+                Console.WriteLine("Error: duplicate tweet");
+                Console.Read();
+            }
         }
 
         private static void SendTweet(string _status)
@@ -49,7 +59,6 @@ namespace TwitterBot
                 }
             });
         }
-
 
         private static String GetTweet(string target)
         {
